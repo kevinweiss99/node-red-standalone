b861ab9a8294f88cd994e4c0573ed94545195b4a
Niklas <n.ande@ostfalia.de>
2025-11-25 11:24:58 +0100

    add exception handling

--- pepper-middleware/rest-server/package/endpoints/robot/audio_player.py
+++ pepper-middleware/rest-server/package/endpoints/robot/audio_player.py
@@ -2,9 +2,10 @@ from flask import request, Response
 import os
 import uuid
 import logging
+import traceback
 
 from ...server import app, socketio
-from ...pepper.connection import audio, audio_player  # audio = ALAudioDevice, audio_player = ALAudioPlayer
+from ...pepper.connection import audio_player
 from ...decorator import log
 
 logger = logging.getLogger(__name__)
@@ -12,6 +13,9 @@ logger = logging.getLogger(__name__)
 @app.route("/robot/audio_player/attention", methods=["GET"])
 @log("/robot/audio_player/attention")
 def play_uploaded_file():
-    audio_player.playFile("home/nao/attention.wav")
+    try:
+        audio_player.playFile("home/nao/attention.wav")
+    except Exception as e:
+        return Response(str(e), 400)        
 
     return Response(status=200)
