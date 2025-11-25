337874db4990215c57792acee7d428ad45ac8a1c
Niklas <n.ande@ostfalia.de>
2025-11-25 12:17:45 +0100

    async audio playback tests

--- pepper-middleware/rest-server/package/endpoints/robot/audio_player.py
+++ pepper-middleware/rest-server/package/endpoints/robot/audio_player.py
@@ -10,11 +10,11 @@ from ...decorator import log
 logger = logging.getLogger(__name__)
 
 
-@app.route("/robot/audio_player/file", methods=["GET"])
-@log("/robot/audio_player/file")
+@app.route("/robot/audio_player/playfile", methods=["GET"])
+@log("/robot/audio_player/playfile")
 def play_existing_file():
     """
-    Example: GET /robot/audio_player/file?filename=attention.wav
+    Example: GET /robot/audio_player/playfile?filename=attention.wav
     Plays /home/nao/attention.wav
     """
     filename = request.args.get("filename")
@@ -23,6 +23,59 @@ def play_existing_file():
 
     try:
         audio_player.playFile(f"/home/nao/{filename}")
+    except Exception as e:
+        return Response(str(e), status=500)
+    return Response(status=200)
+
+@app.route("/robot/audio_player/playfile/async", methods=["GET"])
+@log("/robot/audio_player/playfile/async")
+def play_existing_file_async():
+    """
+    Example: GET /robot/audio_player/playfile/async?filename=attention.wav
+    Plays /home/nao/attention.wav
+    """
+    filename = request.args.get("filename")
+    if not filename:
+        return Response("Missing 'filename' query parameter", status=400)
+
+    try:
+        audio_player.playFile(f"/home/nao/{filename}", _async=True)
+    except Exception as e:
+        return Response(str(e), status=500)
+    return Response(status=200)
+
+@app.route("/robot/audio_player/play", methods=["GET"])
+@log("/robot/audio_player/play")
+def play_existing_file_alt():
+    """
+    Example: GET /robot/audio_player/play?filename=attention.wav
+    Plays /home/nao/attention.wav
+    """
+    filename = request.args.get("filename")
+    if not filename:
+        return Response("Missing 'filename' query parameter", status=400)
+
+    try:
+        fileId = audio_player.loadFile(f"/home/nao/{filename}")
+        audio_player.play(fileId)
+    except Exception as e:
+        return Response(str(e), status=500)
+    return Response(status=200)
+
+@app.route("/robot/audio_player/play/async", methods=["GET"])
+@log("/robot/audio_player/play/async")
+def play_existing_file_alt_async():
+    """
+    Example: GET /robot/audio_player/play/async?filename=attention.wav
+    Plays /home/nao/attention.wav
+    """
+    filename = request.args.get("filename")
+    if not filename:
+        return Response("Missing 'filename' query parameter", status=400)
+
+    try:
+        fileId = audio_player.loadFile(f"/home/nao/{filename}")
+        audio_player.play(fileId, _async=True)
     except Exception as e:
         return Response(str(e), status=500)
     return Response(status=200)
