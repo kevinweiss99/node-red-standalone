f50b8fd6f47494c70766edc5547920f88c307f56
Niklas <n.ande@ostfalia.de>
2025-11-25 11:59:26 +0100

    playing back audio web streams

--- pepper-middleware/rest-server/package/endpoints/robot/audio_player.py
+++ pepper-middleware/rest-server/package/endpoints/robot/audio_player.py
@@ -10,11 +10,11 @@ from ...decorator import log
 logger = logging.getLogger(__name__)
 
 
-@app.route("/robot/audio_player", methods=["GET"])
-@log("/robot/audio_player")
+@app.route("/robot/audio_player/file", methods=["GET"])
+@log("/robot/audio_player/file")
 def play_existing_file():
     """
-    Example: GET /robot/audio_player?filename=attention.wav
+    Example: GET /robot/audio_player/file?filename=attention.wav
     Plays /home/nao/attention.wav
     """
     filename = request.args.get("filename")
@@ -25,5 +25,30 @@ def play_existing_file():
         audio_player.playFile(f"/home/nao/{filename}")
     except Exception as e:
         return Response(str(e), status=500)
+    return Response(status=200)
 
+@app.route("/robot/audio_player/webstream", methods=["GET"])
+@log("/robot/audio_player/webstream")
+def play_webstream():
+    """
+    Example: GET /robot/audio_player/webstream?url=http://stream.antennethueringen.de/live/aac-64/stream.antennethueringen.de/
+    Plays the web radio
+    """
+    url = request.args.get("url")
+    if not url:
+        return Response("Missing 'url' query parameter", status=400)
+
+    try:
+        audio_player.playWebStream(url)
+    except Exception as e:
+        return Response(str(e), status=500)
+    return Response(status=200)
+
+@app.route("/robot/audio_player/stop", methods=["GET"])
+@log("/robot/audio_player/stop")
+def stop_playback():
+    try:
+        audio_player.stopAll()
+    except Exception as e:
+        return Response(str(e), status=500)
     return Response(status=200)
