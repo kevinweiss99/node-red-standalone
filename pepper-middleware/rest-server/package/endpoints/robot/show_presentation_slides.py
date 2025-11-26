4a0b9d33537f3594dd562ebcece523404228901f
Kevin Weiss <weisskevin77@web.de>
2025-11-26 14:50:57 +0100

    adding presentation py

---
+++ pepper-middleware/rest-server/package/endpoints/robot/show_presentation_slides.py
@@ -0,0 +1,53 @@
+import logging
+import urllib.parse
+
+from flask import request, Response
+from flask_socketio import emit
+
+from ...server import app, socketio
+from ...pepper.connection import tablet
+from ...config import FLASK_IP, FLASK_PORT
+from ...decorator import log
+
+logger = logging.getLogger(__name__)
+
+@socketio.on("/robot/presentation/slide")
+@app.route("/robot/presentation/slide")
+@log("/robot/presentation/slide")
+def post_slide_online(url=None):
+    if not url:
+        url = request.get_json(force=True, silent=True)["url"]
+    return Response(status=200)
+
+@socketio.on("/robot/presentation/slide")
+@app.route("/robot/presentation/slide", methods=["POST"])
+@log("/robot/presentation/slide")
+def post_slide_online(url=None):
+    if not url:
+        url = request.get_json(force=True, silent=True)["url"]
+    
+    return Response(status=200)
+
+@socketio.on("/robot/tablet/image")
+@app.route("/robot/tablet/image", methods=["POST"])
+@log("/robot/tablet/image")
+def show_image(url=None):
+    if not url:
+        url = request.get_json(force=True, silent=True)["url"]
+
+    if not tablet.showImage(url, _async=True):
+        logger.warning("Website {} is not reachable.".format(url))
+
+        with app.test_request_context():
+            emit("/robot/tablet/image/error", url, broadcast=True, namespace="/")
+        return Response(status=400)
+
+    return Response(status=200)
+
+@socketio.on("/robot/tablet/clear")
+@app.route("/robot/tablet/clear", methods=["POST"])
+@log("/robot/tablet/clear")
+def clear_tablet():
+    show_default_image()
+
+    return Response(status=200)
