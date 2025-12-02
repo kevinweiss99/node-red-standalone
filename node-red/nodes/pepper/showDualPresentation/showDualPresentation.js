34db5f13c926dec8e6d1f9c95f6ac650a4f7cc8e
Kevin Weiss <weisskevin77@web.de>
2025-12-02 12:19:35 +0100

    still working on dual presentation

--- node-red/nodes/pepper/showDualPresentation/showDualPresentation.js
+++ node-red/nodes/pepper/showDualPresentation/showDualPresentation.js
@@ -6,23 +6,12 @@ module.exports = RED => {
         const node = this;
         const endpoint = "http://172.30.36.198:5001/robot/presentation/show_slide";
         node.on("input", async msg => {
-            try {
-                const response = await got.post(endpoint, {
-                    json: { url: config.url },
-                    responseType: "json",
-                    timeout: 5000
-                });
-
-                node.send({ payload: response.body });
-
-                node.status({ fill: "green", shape: "dot", text: "OK" });
-
-            } catch (err) {
-                node.status({ fill: "red", shape: "ring", text: "POST failed" });
+            got.post(endpoint, {
+                json: { url: config.url }
+            }).catch(err => {
                 node.error(err);
-
-                node.send({ payload: null, error: err });
-            }
+            });
+            node.send(msg);
         });
     }
 
